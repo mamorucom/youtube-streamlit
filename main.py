beba90b70d1f216574bbb849221a2d37869e3e84
mamorucom <eetsutto@gmail.com>
2021-11-16 05:43:51 +0900

    sidebar

--- main.py
+++ main.py
@@ -64,12 +64,12 @@ st.title('Streamlit 超入門')
 st.write('Interactive Widgets')
 
 # テキスト入力
-text = st.text_input('あなたの趣味を教えてください。')
-'あなたの趣味：', text
+# text = st.text_input('あなたの趣味を教えてください。')
+# 'あなたの趣味：', text
 
 # スライダー
-condition = st.slider('あなたの今の調子は？',0,100,50)
-'コンディション：', condition
+# condition = st.slider('あなたの今の調子は？',0,100,50)
+# 'コンディション：', condition
 
 
 # セレクトボックス
@@ -83,3 +83,10 @@ if st.checkbox('Show Image'):
     img = Image.open('sample.jpg')
     # use_column_width: 実際のレイアウトの横幅に合わせる
     st.image(img, caption='baby', use_column_width=True)
+
+# サイドバー
+text = st.sidebar.text_input('あなたの趣味を教えてください。')
+condition = st.sidebar.slider('あなたの今の調子は？',0,100,50)
+
+'あなたの趣味：', text
+'コンディション：', condition
