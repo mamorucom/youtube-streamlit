600eb4580df8d363c710a43256eef4e8197ccff4
mamorucom <eetsutto@gmail.com>
2021-11-16 06:25:16 +0900

    外部ライブラリ宣言

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 # from tkinter import *
-from PIL import Image
+# from PIL import Image
 import time
 
 # import PIL.Image
