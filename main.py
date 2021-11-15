7a6f7b46e4ac2d39f20ee19124a340dd52566234
mamorucom <eetsutto@gmail.com>
2021-11-16 06:10:19 +0900

    プログレスバー

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 # from tkinter import *
 from PIL import Image
+import time
 
 # import PIL.Image
 
@@ -90,6 +91,20 @@ if st.checkbox('Show Image'):
 
 # 'あなたの趣味：', text
 # 'コンディション：', condition
+
+# プログレスバー
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!'
 
 # 2カラム
 left_column, right_column = st.columns(2)
